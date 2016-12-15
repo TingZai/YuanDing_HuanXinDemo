9a60c996c506b0c34660078c6a5e01c6db3fa494
yuting <y_t209@163.com>
2016-12-15 14:45:04 +0800

    添加工程

---
+++ YuanDing_HuanXin/YuanDing_HuanXin/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YuanDing_HuanXin
+//
+//  Created by 余婷 on 2016/12/15.
+//  Copyright © 2016年 余婷. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
